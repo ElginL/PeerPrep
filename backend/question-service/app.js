cf6aa7d4f9f439d6a82068609ff755baba79375f
ElginL <elginlee_2000@hotmail.com>
2023-09-03 17:27:53 +0800

    Initialize question-service with fetch all and post endpoints

---
+++ backend/question-service/app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+
+const app = express();
+
+const questions = [
+    { id: 1, title: "Reverse a string", category: "Strings, Algorithms", complexity: "Easy", description: "A very long description" },
+    { id: 2, title: "Linked List Cycle Detection", category: "Data Structures, Algorithms", complexity: "Easy", description: "Another long description" },
+    { id: 3, title: "Rotate Image", category: "Arrays, Algorithms", complexity: "Medium", description: "hello world description" },
+    { id: 4, title: "N-Queen Problem", category: "Algorithms", complexity: "Hard", description: "The N queen problem description" }
+]
+
+// middleware to parse json
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.json(questions);
+});
+
+app.post('/', (req, res) => {
+    const newQuestion = req.body;
+    console.log(newQuestion);
+
+    questions.push(newQuestion);
+    res.send("New question is saved");
+});
+
+app.listen(3001, () => {
+    console.log('Server is running on http://localhost:3001')
+})
