d0ae9af1aa497fabd56bd615a672c1ba102320ad
ElginL <elginlee_2000@hotmail.com>
2023-09-04 11:48:09 +0800

    Modify AddQuestionModal to have 3 fields

--- frontend/src/components/AddQuestionModal.js
+++ frontend/src/components/AddQuestionModal.js
@@ -5,8 +5,27 @@ const AddQuestionModal = ({ isVisible, setIsVisible }) => {
 
     return (
         <div className={styles[containerStyle]} onClick={() => setIsVisible(false)}>
-            <div className={styles["form"]} onClick={e => e.stopPropagation()}>
+            <div className={styles["form-container"]} onClick={e => e.stopPropagation()}>
+                <h2>Add Question</h2>
+                <form className={styles["form"]}>
+                    <div className={styles["input-container"]}>
+                        <label htmlFor="title">Title:</label>
+                        <input type="text" placeholder="New Title" name="title" />
+                    </div>
+                    <div className={styles["input-container"]}>
+                        <label htmlFor="category">Category:</label>
+                        <input type="text" placeholder="Category (e.g. Binary Search)" name="category" />
+                    </div>
+                    <div className={styles["input-container"]}>
+                        <label htmlFor="complexity">Complexity:</label>
+                        <select name="complexity">
+                            <option value="Easy">Easy</option>
+                            <option value="Medium">Medium</option>
+                            <option value="Hard">Hard</option>
+                        </select>
+                    </div>
 
+                </form>
             </div>
         </div>
     )
