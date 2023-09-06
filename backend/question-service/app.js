38498d4c79c5c7715bb8dc1c5d836d783289cb1e
ElginL <elginlee_2000@hotmail.com>
2023-09-07 02:19:53 +0800

    Set up mongodb connection

--- backend/question-service/app.js
+++ backend/question-service/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const questionRoute = require('./routes/questionRoute');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 const app = express();
 
@@ -14,6 +16,21 @@ const corsOption = {
     methods: 'GET, POST, DELETE'
 };
 app.use(cors(corsOption));
+
+// db connection
+mongoose.connect(
+    process.env.MONGODB_URL,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+);
+
+const db = mongoose.connection;
+db.on('error', console.error.bind("connection error: "));
+db.once('open', function() {
+    console.log("Connected successfully");
+});
 
 app.listen(3001, () => {
     console.log('Server is running on http://localhost:3001')
