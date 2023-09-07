cfa3093be84d9af526ed29f807d75a078e55d3dd
ElginL <elginlee_2000@hotmail.com>
2023-09-08 02:52:42 +0800

    Add jwt authentication middleware for deregister endpoint

--- backend/user-service/routes/userRoute.js
+++ backend/user-service/routes/userRoute.js
@@ -2,6 +2,7 @@ const express = require('express');
 const userRouter = express.Router();
 const userController = require('../controllers/userController');
 const { validateUser } = require('../middleware/UserValidator');
+const authenticateJwt = require('../middleware/authenticateJwt');
 
 userRouter.get('/', (req, res, next) => {
     res.json({ "messsage": "Hello "});
@@ -11,6 +12,6 @@ userRouter.post('/register', validateUser(), userController.createUser);
 
 userRouter.post('/login', userController.loginUser);
 
-userRouter.delete('/deregister', userController.deleteUser);
+userRouter.delete('/deregister', authenticateJwt, userController.deleteUser);
 
 module.exports = userRouter;
