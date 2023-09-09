f65dc7d3949a1588da4ce22b95db3057999b693a
ElginL <elginlee_2000@hotmail.com>
2023-09-09 15:30:47 +0800

    Add username and password length requirement

--- backend/user-service/middleware/userValidator.js
+++ backend/user-service/middleware/userValidator.js
@@ -5,19 +5,24 @@ const validateUser = () => {
     return [
         body('username')
             .notEmpty().withMessage("Username is required")
+            .isLength({ min: 5, max: 12}).withMessage("Username must be between 5 and 12 characters")
             .custom(async value => {
                 if (await User.findOne({ where: { username: value } })) {
                     throw new Error('Username is already in use')
                 }
             }),
-        body('password').notEmpty().withMessage("Password is required"),
+        body('password')
+            .notEmpty().withMessage("Password is required")
+            .isLength({ min: 8, max: 20 }).withMessage("Password must be between 8 and 20 characters"),
         handleErrors
     ];
 };
 
 const validateNewPassword = () => {
     return [
-        body('newPassword').notEmpty().withMessage("Password is required"),
+        body('newPassword')
+            .notEmpty().withMessage("Password is required")
+            .isLength({ min: 8, max: 20 }).withMessage("Password must be between 8 and 20 characters"),
         handleErrors
     ];
 };
