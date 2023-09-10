89fb9796acbca51ff6c02008bb3531b2ad1aefbe
ElginL <elginlee_2000@hotmail.com>
2023-09-10 15:19:01 +0800

    Fix unauthenticated routes

--- frontend/src/routes/UnauthenticatedRoutes.js
+++ frontend/src/routes/UnauthenticatedRoutes.js
@@ -1,7 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import HomePage from "../pages/Home";
-import QuestionPage from "../pages/QuestionPage";
 import LogInPage from "../pages/LogIn";
 import SignUpPage from "../pages/SignUp";
 
@@ -10,7 +8,6 @@ const UnauthenticatedRoutes = () => {
     <Router>
       <Routes>
         <Route path="/" element={<LogInPage />} />
-        <Route path="/questions/:id" element={<QuestionPage />} />
         <Route path="/signup" element={<SignUpPage />} />
       </Routes>
     </Router>
