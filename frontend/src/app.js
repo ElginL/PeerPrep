2678597c729adec686bd27f83731c2e948338a55
ElginL <elginlee_2000@hotmail.com>
2023-09-23 01:11:04 +0800

    Add loading status

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,10 +1,30 @@
+import { useEffect, useState } from "react";
 import AuthenticatedRoutes from "./routes/AuthenticatedRoutes";
 import UnauthenticatedRoutes from "./routes/UnauthenticatedRoutes";
-import { useRecoilValue } from "recoil";
-import { isLoggedInSelector } from './recoil/UserSelector'
+import { useRecoilState } from "recoil";
+import { isLoggedInState } from './recoil/UserAtom';
+import styles from './styles/app.module.css';
 
 const App = () => {
-    const isLoggedIn = useRecoilValue(isLoggedInSelector);
+    const [isLoggedIn, setIsLoggedIn] = useRecoilState(isLoggedInState);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        setTimeout(() => {
+            if (localStorage.getItem('credentials')) {
+                setIsLoggedIn(true);
+            }
+            setIsLoading(false);
+        }, 1000);
+    }, [setIsLoggedIn]);
+
+    if (isLoading) {
+        return (
+            <div className={styles["loading"]}>
+                Loading...
+            </div>
+        );
+    }
 
     return (
         <div>
