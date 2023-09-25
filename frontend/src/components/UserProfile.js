4f7e5039e3c53b33b7a080743db8d30712f8e9c6
e1010101 <ezratio15195@gmail.com>
2023-09-25 14:37:35 +0800

    Mark UserProfile.js as defunct

--- frontend/src/components/UserProfile.js
+++ frontend/src/components/UserProfile.js
@@ -1,21 +1,19 @@
-import { useState, useEffect } from 'react';
-import styles from '../styles/components/UserProfile.module.css';
-import { Link } from 'react-router-dom';
-import { getUsername } from '../api/users';
+import { useState, useEffect } from "react";
+import styles from "../styles/components/UserProfile.module.css";
+import { Link } from "react-router-dom";
+import { getUsername } from "../api/users";
 
+// DEFUNCT
 const UserProfile = () => {
-    const [username, setUsername] = useState('');
+    const [username, setUsername] = useState("");
 
     useEffect(() => {
-        getUsername()
-            .then(res => setUsername(res));
+        getUsername().then((res) => setUsername(res));
     }, []);
 
     return (
         <div className={styles["container"]}>
-            <h2 className={styles["username"]}>
-                {username}
-            </h2>
+            <h2 className={styles["username"]}>{username}</h2>
             <Link to="/profile" className={styles["edit-profile-btn"]}>
                 Edit Profile
             </Link>
