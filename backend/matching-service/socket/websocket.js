e6c404d3efe3ef00c18bf1a74e1e7e51874b1d7d
e1010101 <ezratio15195@gmail.com>
2023-10-06 00:19:31 +0800

    Add logs for joining/leaving queue

--- backend/matching-service/socket/websocket.js
+++ backend/matching-service/socket/websocket.js
@@ -1,14 +1,14 @@
-const { instrument } = require('@socket.io/admin-ui')
-const jwt = require('jsonwebtoken');
-const { v4: uuidv4 } = require('uuid');
-require('dotenv').config();
-const waitingListRepo = require('../db/repositories/waitingListRepo');
+const { instrument } = require("@socket.io/admin-ui");
+const jwt = require("jsonwebtoken");
+const { v4: uuidv4 } = require("uuid");
+require("dotenv").config();
+const waitingListRepo = require("../db/repositories/waitingListRepo");
 
-const initializeWebSocket = server => {
-    const io = require('socket.io')(server, {
+const initializeWebSocket = (server) => {
+    const io = require("socket.io")(server, {
         cors: {
-            origin: ['http://localhost:3000', 'https://admin.socket.io'],
-            credentials: true
+            origin: ["http://localhost:3000", "https://admin.socket.io"],
+            credentials: true,
         },
     });
 
@@ -17,7 +17,7 @@ const initializeWebSocket = server => {
 
         jwt.verify(token, process.env.JWT_SECRET_KEY, (err, decoded) => {
             if (err) {
-                return next(new Error('Authentication Error'));
+                return next(new Error("Authentication Error"));
             }
 
             socket.username = decoded.username;
@@ -25,36 +25,43 @@ const initializeWebSocket = server => {
         });
     });
 
-    io.on('connection', socket => {
+    io.on("connection", (socket) => {
         console.log(`User connected: ${socket.id}`);
 
-        socket.on('disconnect', () => {
+        socket.on("disconnect", () => {
+            console.log(socket.username + " removed from queue");
             console.log(`User disconnected: ${socket.id}`);
             waitingListRepo.deleteByUsername(socket.username);
         });
-        
-        socket.on('joinQueue', async queueComplexity => {
-            const otherUser = await waitingListRepo.getByComplexity(queueComplexity);
+
+        socket.on("joinQueue", async (queueComplexity) => {
+            const otherUser = await waitingListRepo.getByComplexity(
+                queueComplexity
+            );
 
             // No other user queuing for that complexity yet.
             if (otherUser == null) {
                 const roomId = uuidv4();
                 socket.join(roomId);
-                waitingListRepo.addEntry(socket.username, queueComplexity, roomId);
+                console.log(socket.username + " added to queue");
+                waitingListRepo.addEntry(
+                    socket.username,
+                    queueComplexity,
+                    roomId
+                );
                 return;
             }
 
             const sharedRoomId = otherUser.roomId;
             socket.join(sharedRoomId);
 
-            io.to(socket.id).emit('createRoom', sharedRoomId, queueComplexity);
-            io.to(sharedRoomId).emit('matchfound', sharedRoomId);
+            io.to(socket.id).emit("createRoom", sharedRoomId, queueComplexity);
+            io.to(sharedRoomId).emit("matchfound", sharedRoomId);
         });
     });
 
     instrument(io, { auth: false });
 };
-
 
 module.exports = {
     initializeWebSocket,
