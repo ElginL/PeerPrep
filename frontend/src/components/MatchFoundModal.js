fedc908a48ca722c56026d72587c809ccfe34165
ElginL <elginlee_2000@hotmail.com>
2023-10-19 02:02:10 +0800

    Update match found / retry modal

--- frontend/src/components/MatchFoundModal.js
+++ frontend/src/components/MatchFoundModal.js
@@ -1,29 +1,54 @@
-import { useEffect } from 'react';
-import styles from '../styles/components/MatchFoundModal.module.css';
+import * as React from 'react';
+import Backdrop from '@mui/material/Backdrop';
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import Fade from '@mui/material/Fade';
+import Typography from '@mui/material/Typography';
 
-const MatchFoundModal = ({ 
-    isVisible
-}) => {
-    const containerStyle = isVisible ? "container-visible" : "container-not-visible";
-
-    useEffect(() => {
-        if (isVisible) {
-            document.body.style.overflow = 'hidden';
-            document.documentElement.style.overflow = 'hidden';
-        } else {
-            document.body.style.overflow = 'auto';
-            document.documentElement.style.overflow = 'auto';
-        }
-    }, [isVisible]);
-
-    return (
-        <div className={styles[containerStyle]}>
-            <div className={styles["main-container"]}>
-                <h1>Match Found</h1>
-                <p>Redirecting to room...</p>
-            </div>
-        </div>
-    )
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+  color: 'black',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center'
 };
+
+const MatchFoundModal = ({ isVisible }) => {
+  return (
+    <div>
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        open={isVisible}
+        closeAfterTransition
+        slots={{ backdrop: Backdrop }}
+        slotProps={{
+          backdrop: {
+            timeout: 500,
+          },
+        }}
+      >
+        <Fade in={isVisible}>
+          <Box sx={style}>
+            <Typography id="transition-modal-title" variant="h6" component="h2">
+              Match Found!
+            </Typography>
+            <Typography id="transition-modal-description" sx={{ mt: 2 }}>
+              Redirecting to room...
+            </Typography>
+          </Box>
+        </Fade>
+      </Modal>
+    </div>
+  );
+}
 
 export default MatchFoundModal;
