4c58f4fd92e59989734ae3ecf2078722475aa073
ElginL <elginlee_2000@hotmail.com>
2023-11-01 13:50:25 +0800

    Add username render in profile page

--- frontend/src/pages/EditProfile.js
+++ frontend/src/pages/EditProfile.js
@@ -1,13 +1,27 @@
+import { useState, useEffect } from "react";
 import Navbar from "../components/Navbar";
 import DeregisterAccount from "../components/DeregisterAccount";
 import ChangePassword from "../components/ChangePassword";
 import styles from "../styles/pages/EditProfile.module.css";
+import { getUsername } from "../api/users";
 
 const EditProfile = () => {
+  const [username, setUsername] = useState("");
+
+  useEffect(() => {
+    const fetchUsername = async () => {
+      const res = await getUsername();
+      setUsername(res);
+    }
+  
+    fetchUsername();
+  }, []);
+
   return (
     <div>
       <Navbar />
       <div className={styles["container"]}>
+        <h2>{username}'s Profile</h2>
         <ChangePassword />
         <DeregisterAccount />
       </div>
