9f8b9dfb61f272d172efe57ed0dd577929c81788
jgyj123 <jonathangohyj@gmail.com>
2023-11-10 21:59:16 +0800

    add markdown parsing for Ai Chat

--- frontend/src/components/AiChat.js
+++ frontend/src/components/AiChat.js
@@ -1,16 +1,41 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import axios from "axios";
 import Draggable from "react-draggable";
 import styles from "../styles/components/AiChat.module.css";
+import Markdown from "marked-react";
 
 function AiChat() {
   const [message, setMessage] = useState("");
-  const [chatHistory, setChatHistory] = useState([
-    // ... your mock chat history remains unchanged
-  ]);
+  const [chatHistory, setChatHistory] = useState([]);
   const [isChatVisible, setChatVisible] = useState(false);
   const [isAwaitingReply, setIsAwaitingReply] = useState(false);
+  const [isDraggable, setIsDraggable] = useState(true);
+  const messagesEndRef = useRef(null);
 
+  const scrollToBottom = () => {
+    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
+
+  // Scroll to bottom whenever chatHistory changes
+  useEffect(() => {
+    scrollToBottom();
+  }, [chatHistory]);
+  // Function to disable dragging
+  const disableDragging = () => {
+    setIsDraggable(false);
+  };
+
+  // Function to enable dragging
+  const enableDragging = () => {
+    setIsDraggable(true);
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter" && !e.shiftKey) {
+      e.preventDefault(); // Prevents the default action of inserting a new line
+      handleChat();
+    }
+  };
   const handleChat = async () => {
     try {
       const userMessage = { role: "user", content: message };
@@ -44,9 +69,9 @@ function AiChat() {
 
   return (
     <div className={styles.overlay}>
-      <Draggable>
+      <Draggable disabled={!isDraggable}>
         <div className={styles["chat-container"]}>
-          <div className={styles.header}>
+          <div className={styles.header} onMouseDown={enableDragging}>
             <span className={styles.title}>Chat Bot</span>
             <button
               className={styles["close-btn"]}
@@ -55,22 +80,24 @@ function AiChat() {
               &times;
             </button>
           </div>
-          <div>
+          <div onMouseDown={disableDragging}>
             {chatHistory.map((msg, index) => (
               <p key={index} className={styles[msg.role]}>
                 {msg.role === "user" ? "You: " : "System: "}
-                {msg.content}
+                <Markdown>{msg.content}</Markdown>
               </p>
             ))}
             {isAwaitingReply && (
               <p className={styles.system}>System: Please wait...</p>
             )}
+            <div ref={messagesEndRef} />
           </div>
           <div className={styles["message-input"]}>
             <textarea
               className={styles["textarea-field"]}
               value={message}
               onChange={(e) => setMessage(e.target.value)}
+              onKeyPress={handleKeyPress}
               placeholder="Type your message..."
             ></textarea>
             <button className={styles["send-btn"]} onClick={handleChat}>
